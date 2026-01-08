b7d5dcf534a2a2ccbd939d22ffafae6e96803505
John Byrd <johnwbyrd@gmail.com>
2026-01-08 01:59:06 -0800

    feat(mos): add setjmp.h header and build configuration
    This commit introduces the `setjmp.h` header file for the MOS 6502
    architecture. The `jmp_buf` is defined as an array of 19 bytes to
    accommodate the return address, hardware stack pointer, soft stack
    pointer, and callee-saved registers.
    
    Additionally, the Meson build system is updated to include the new
    `machine` subdirectory and install the header file.

---
+++ newlib/libc/machine/mos/machine/setjmp.h
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright © 2025 John Byrd
+ *
+ * MOS 6502 setjmp buffer definition.
+ *
+ * jmp_buf layout (19 bytes):
+ *   [0-1]   Return address (from hardware stack)
+ *   [2]     Hardware stack pointer
+ *   [3-4]   Soft stack pointer (__rc0/__rc1)
+ *   [5-18]  Callee-saved registers (__rc18-__rc31)
+ */
+
+#ifndef _MACHINE_SETJMP_H_
+#define _MACHINE_SETJMP_H_
+
+#define _JBLEN 19
+#define _JBTYPE unsigned char
+
+typedef _JBTYPE jmp_buf[_JBLEN];
+
+#endif /* _MACHINE_SETJMP_H_ */
